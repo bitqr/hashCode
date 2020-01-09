cbd2876d6383bbbd7466ac12acc131584969685f
bitqr <abdlbitar@gmail.com>
2020-01-09 02:23:04 +0100

    Added Dynamic Programming method (pseudo-polynomial algorithm), optimality reached for example, small and medium

--- 2020/practice_round/src/main.py
+++ 2020/practice_round/src/main.py
@@ -5,7 +5,7 @@ from solution import Solution
 inputs_array = ['a_example.in', 'b_small.in', 'c_medium.in', 'd_quite_big.in', 'e_also_big.in']
 outputs_array = ['a_example.out', 'b_small.out', 'c_medium.out', 'd_quite_big.out', 'e_also_big.out']
 
-instance_index = 4
+instance_index = 2
 
 file_name = '../inputs/' + inputs_array[instance_index]
 
@@ -15,7 +15,9 @@ print(instance.to_string())
 solver = Solver(instance)
 
 # found = solver.solve()
-found = solver.greedy()
+# found = solver.greedy()
+found = solver.dynamic_programming_method()
+print(found)
 
 solution = Solution(instance, found)
 print("Score =", solution.compute_score())
--- 2020/practice_round/src/solver.py
+++ 2020/practice_round/src/solver.py
@@ -5,6 +5,8 @@ class Solver:
 
     def __init__(self, instance: Instance):
         self.instance = instance
+        self.sub_problems = dict()
+        self.sub_problems_decision = dict()
 
     def solve(self) -> [int]:
         """Silly solving method"""
@@ -31,4 +33,47 @@ class Solver:
                 current_capacity -= self.instance.slices_per_type[index]
             if current_capacity == 0:
                 return result
+        return sorted(result)
+
+    def get_sub_problem_value(self, type_index: int, max_slices_capacity: int) -> int:
+        if self.sub_problems.get((type_index, max_slices_capacity)) is not None:
+            return self.sub_problems.get((type_index, max_slices_capacity))
+        # Not found. Compute the value for the first time
+        # Base case
+        if type_index == 0:
+            self.sub_problems[(type_index, max_slices_capacity)] = self.instance.slices_per_type[0] if \
+                self.instance.slices_per_type[0] <= max_slices_capacity else 0
+            self.sub_problems_decision[(type_index, max_slices_capacity)] = \
+                (self.sub_problems[(type_index, max_slices_capacity)] != 0)
+            return self.sub_problems[(type_index, max_slices_capacity)]
+        # Recursive case
+        slices_count_at_index = self.instance.slices_per_type[type_index]
+        if slices_count_at_index <= max_slices_capacity:
+            self.sub_problems[(type_index, max_slices_capacity)] = \
+                max(self.get_sub_problem_value(type_index-1, max_slices_capacity),
+                    self.get_sub_problem_value(type_index-1, max_slices_capacity-slices_count_at_index)
+                    + slices_count_at_index)
+            self.sub_problems_decision[(type_index, max_slices_capacity)] = \
+                (self.sub_problems[(type_index, max_slices_capacity)] !=
+                 self.get_sub_problem_value(type_index-1, max_slices_capacity))
+            return self.sub_problems[(type_index, max_slices_capacity)]
+        else:
+            self.sub_problems[(type_index, max_slices_capacity)] = \
+                self.get_sub_problem_value(type_index-1, max_slices_capacity)
+            self.sub_problems_decision[(type_index, max_slices_capacity)] = False
+            return self.sub_problems[(type_index, max_slices_capacity)]
+
+    def dynamic_programming_optimal_value(self) -> int:
+        return self.get_sub_problem_value(self.instance.max_types-1, self.instance.max_slices)
+
+    def dynamic_programming_method(self) -> [int]:
+        self.dynamic_programming_optimal_value()
+        result = []
+        t = self.instance.max_types-1
+        c = self.instance.max_slices
+        while t >= 0:
+            if self.sub_problems_decision[(t, c)]:
+                result.append(t)
+                c -= self.instance.slices_per_type[t]
+            t = t-1
         return sorted(result)

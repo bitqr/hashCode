7a6bbd502f3aa055b72ff2e53fb9687d9d50b3bc
bitqr <abdlbitar@gmail.com>
2020-02-12 21:32:05 +0100

    Added input files for practice round

--- 2020/practice_round/src/main.py
+++ 2020/practice_round/src/main.py
@@ -2,23 +2,24 @@ from instance import Instance
 from solver import Solver
 from solution import Solution
 
-inputs_array = ['a_example.in', 'b_small.in', 'c_medium.in', 'd_quite_big.in', 'e_also_big.in']
-outputs_array = ['a_example.out', 'b_small.out', 'c_medium.out', 'd_quite_big.out', 'e_also_big.out']
+if __name__ == '__main__':
+    inputs_array = ['a_example.in', 'b_small.in', 'c_medium.in', 'd_quite_big.in', 'e_also_big.in']
+    outputs_array = ['a_example.out', 'b_small.out', 'c_medium.out', 'd_quite_big.out', 'e_also_big.out']
 
-instance_index = 0
+    instance_index = 0
 
-file_name = '../inputs/' + inputs_array[instance_index]
+    file_name = '../input/' + inputs_array[instance_index]
 
-instance = Instance(file_name)
-print(instance.to_string())
+    instance = Instance(file_name)
+    print(instance.to_string())
 
-solver = Solver(instance)
+    solver = Solver(instance)
 
-# found = solver.solve()
-# found = solver.greedy()
-found = solver.dynamic_programming_method()
-print(found)
+    # found = solver.solve()
+    # found = solver.greedy()
+    found = solver.dynamic_programming_method()
+    print(found)
 
-solution = Solution(instance, found)
-print("Score =", solution.compute_score())
-solution.write('../outputs/' + outputs_array[instance_index])
+    solution = Solution(instance, found)
+    print("Score =", solution.compute_score())
+    solution.write('../output/' + outputs_array[instance_index])

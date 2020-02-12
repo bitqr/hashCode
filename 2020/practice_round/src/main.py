0956bd346cab3c74cfd187f4b9b7375019138515
bitqr <abdlbitar@gmail.com>
2020-02-12 21:38:23 +0100

    Added a comment about dynamic programming method

--- 2020/practice_round/src/main.py
+++ 2020/practice_round/src/main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     inputs_array = ['a_example.in', 'b_small.in', 'c_medium.in', 'd_quite_big.in', 'e_also_big.in']
     outputs_array = ['a_example.out', 'b_small.out', 'c_medium.out', 'd_quite_big.out', 'e_also_big.out']
 
-    instance_index = 0
+    instance_index = 4
 
     file_name = '../input/' + inputs_array[instance_index]
 
@@ -16,8 +16,10 @@ if __name__ == '__main__':
     solver = Solver(instance)
 
     # found = solver.solve()
-    # found = solver.greedy()
-    found = solver.dynamic_programming_method()
+    found = solver.greedy()
+
+    # Do not use it on big instances
+    # found = solver.dynamic_programming_method()
     print(found)
 
     solution = Solution(instance, found)
